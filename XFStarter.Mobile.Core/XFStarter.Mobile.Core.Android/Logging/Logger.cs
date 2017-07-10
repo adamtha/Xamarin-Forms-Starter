df984cae6a424b464f2bec8863ad201bef7764f3
Adamtha <adamtha@gmail.com>
2017-07-10 16:19:18 +0300

    updated logging

--- XFStarter.Mobile.Core/XFStarter.Mobile.Core.Android/Logging/Logger.cs
+++ XFStarter.Mobile.Core/XFStarter.Mobile.Core.Android/Logging/Logger.cs
@@ -29,12 +29,6 @@ namespace XFStarter.Mobile.Core.Android.Logging
             Name = name;
         }
 
-        public ILogger ForThisClass([CallerMemberName] string memberName = "")
-        {
-            Name = memberName;
-            return this;
-        }
-
         public void WriteLog(LogLevel logLevel, string message, [CallerMemberName] string memberName = "")
         {
             switch(logLevel)
