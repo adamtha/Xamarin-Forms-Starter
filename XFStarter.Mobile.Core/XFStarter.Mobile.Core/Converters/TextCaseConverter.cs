0e9bd32fee43509720bf83b1beb55e26cc96897d
Adamtha <adamtha@gmail.com>
2017-09-19 16:48:38 +0300

    added simple text case converter

---
+++ XFStarter.Mobile.Core/XFStarter.Mobile.Core/Converters/TextCaseConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFStarter.Mobile.Core.Converters
+{
+    public enum TextCase
+    {
+        None,
+        LowerCase,
+        UpperCase
+        //TitleCase
+    }
+
+    public class TextCaseConverter : IValueConverter
+    {
+        public TextCase ToCase { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = value as string;
+            if(text == null)
+            {
+                return value;
+            }
+
+            switch(ToCase)
+            {
+                case TextCase.LowerCase: return culture.TextInfo.ToLower(text);
+                case TextCase.UpperCase: return culture.TextInfo.ToUpper(text);
+                //case TextCase.TitleCase: return culture.TextInfo.ToTitleCase(text);
+                default:
+                    return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
